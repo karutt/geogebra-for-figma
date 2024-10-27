fec1ee02dfa57cd04a3b32574e6f768b9f2df3ef
karutt <karutetto332@gmail.com>
2024-10-27 18:40:05 +0900

    ユーザーの設定してるカラーモードになるように修正

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,12 +1,28 @@
 "use client";
 
-import { Reshaped } from "reshaped";
-import "reshaped/themes/reshaped/theme.css";
+import { Reshaped, useTheme } from "reshaped";
+
 import "reshaped/themes/figma/theme.css";
+import { useEffect } from "react";
+
+// OSのカラーモードを取得する関数
+const detectSystemColorMode = () => {
+    if (typeof window !== "undefined" && window.matchMedia) {
+        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    }
+    return "light"; // デフォルトで"light"を返す
+};
 
 const App = ({ children }) => {
+    const { setColorMode } = useTheme(); // カラーモード操作用フック
+
+    useEffect(() => {
+        const initialMode = detectSystemColorMode(); // OSからカラーモード取得
+        setColorMode(initialMode); // 初期カラーモードを設定
+    }, [setColorMode]); // 依存関係にsetColorModeを指定
+
     return (
-        <Reshaped theme='figma' defaultColorMode='dark'>
+        <Reshaped theme='figma' defaultColorMode={detectSystemColorMode()}>
             {children}
         </Reshaped>
     );
